bbd6b7dd0db559a10ca24433ec0e85eab4c215ec
123shang60 <616761475@qq.com>
2021-09-20 13:23:24 +0800

    feat : agent 注册逻辑

--- cmd/agent.go
+++ cmd/agent.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/123shang60/image-load/pkg/register"
+	"github.com/123shang60/image-load/pkg/router"
+	"github.com/sirupsen/logrus"
+	"os"
+)
 
+func main() {
+	// 定时注册启动
+	go register.RegistAgent()
+	addr := os.Getenv("addr")
+	if addr== "" {
+		addr = "127.0.0.1"
+	}
+	port := os.Getenv("port")
+	if port== "" {
+		port = "8081"
+	}
+	r := router.NewAgentRouter()
+	err := r.Run(addr + ":" + port)
+	if err != nil {
+		logrus.Panic(err)
+	}
 }
---
+++ pkg/router/agent.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func NewAgentRouter() *gin.Engine {
+	r := gin.Default()
+
+	return r
+}
