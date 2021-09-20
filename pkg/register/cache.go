6ae8cf873e92625816d76dfd6bf40ad0f0561ce4
123shang60 <616761475@qq.com>
2021-09-20 12:50:09 +0800

    feat : 注册及查询接口

---
+++ pkg/register/cache.go
@@ -0,0 +1,27 @@
+package register
+
+import (
+	"github.com/patrickmn/go-cache"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+// 注册信息
+var c *cache.Cache
+
+func init() {
+	c = cache.New(30*time.Second, 10*time.Second)
+}
+
+func AddCache(info NodeInfo) error {
+	logrus.Debug("有一个 agent 前来注册。。。。", info)
+
+	c.SetDefault(info.Name, info)
+
+	logrus.Debug("服务注册成功！", info)
+	return nil
+}
+
+func GetCache() map[string]cache.Item {
+	return c.Items()
+}
